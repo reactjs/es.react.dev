fca4773d3dabed88fac6bb0478547efb780a6d62
Nancy Zitle Juárez <ancizj@hotmail.com>
2019-02-16 23:39:03 -0600

    Translate LayoutFooter and LayoutHeader (#160)

--- src/components/LayoutHeader/DocSearch.js
+++ src/components/LayoutHeader/DocSearch.js
@@ -104,7 +104,7 @@ class DocSearch extends Component<{}, State> {
           }}
           id="algolia-doc-search"
           type="search"
-          placeholder="Search docs"
+          placeholder="Buscar en la documentación"
           aria-label="Search docs"
         />
       </form>
