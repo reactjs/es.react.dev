b432a876f98ae76bdda73554f77e5b0e47429ca4
Miguel Alejandro Bolivar Portilla <MBolivar360@gmail.com>
2019-02-20 23:06:45 -0600

    Update examples/context/reference-caveats-problem.js
    Co-Authored-By: ricardoerl <ricardo.ramirezlara@gmail.com>

--- examples/context/reference-caveats-problem.js
+++ examples/context/reference-caveats-problem.js
@@ -2,7 +2,7 @@ class App extends React.Component {
   render() {
     // highlight-range{2}
     return (
-      <Provider value={{something: 'algo'}}>
+      <Provider value={{something: 'something'}}>
         <Toolbar />
       </Provider>
     );
