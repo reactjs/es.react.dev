b474ad65751e79216ae64130b28bb82bf62ceeff
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2022-04-03 20:59:27 -0400

    translate Quick Start (beta docs) (#507)

--- beta/src/components/MDX/MDXComponents.tsx
+++ beta/src/components/MDX/MDXComponents.tsx
@@ -162,7 +162,7 @@ function YouWillLearn({
   children: any;
   isChapter?: boolean;
 }) {
-  let title = isChapter ? 'In this chapter' : 'You will learn';
+  let title = isChapter ? 'En este capítulo' : 'Aprenderás';
   return <SimpleCallout title={title}>{children}</SimpleCallout>;
 }
 
