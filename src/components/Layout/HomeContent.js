6c5d08d406b7c1c6fa5feeee39679359e56fcfe4
Néstor Guillermo Hernández <nghsoft@fibertel.com.ar>
2023-08-14 14:10:33 -0300

    Update src/components/Layout/HomeContent.js

--- src/components/Layout/HomeContent.js
+++ src/components/Layout/HomeContent.js
@@ -503,7 +503,6 @@ export function HomeContent() {
           <div className="mt-20 px-5 lg:px-0 mb-6 max-w-4xl text-center text-opacity-80">
             <Logo className="text-link dark:text-link-dark w-24 lg:w-28 mb-10 lg:mb-8 mt-12 h-auto mx-auto self-start" />
             <Header>
-<<<<<<< HEAD
               Bienvenido a la <br className="hidden lg:inline" />
               comunidad de React
 >>>>>>> 819518cfe32dd2db3b765410247c30feea713c77
