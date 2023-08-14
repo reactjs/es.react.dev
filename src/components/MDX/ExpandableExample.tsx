d0e66f7f5d08da662f5d74cea66a207c958a6f6c
Néstor Guillermo Hernández <nghsoft@fibertel.com.ar>
2023-08-14 15:01:19 -0300

    Update src/components/MDX/ExpandableExample.tsx

--- src/components/MDX/ExpandableExample.tsx
+++ src/components/MDX/ExpandableExample.tsx
@@ -71,7 +71,6 @@ function ExpandableExample({children, excerpt, type}: ExpandableExampleProps) {
           {isDeepDive && (
             <>
 <<<<<<< HEAD
-=======
               <IconDeepDive className="inline me-2 dark:text-purple-30 text-purple-40" />
               Profundizar
 >>>>>>> 819518cfe32dd2db3b765410247c30feea713c77
