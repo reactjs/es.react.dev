0d00399abe38d8ea729afde01c29455330746593
Néstor Guillermo Hernández <nghsoft@fibertel.com.ar>
2023-08-14 15:29:57 -0300

    Update src/components/MDX/YouWillLearnCard.tsx

--- src/components/MDX/YouWillLearnCard.tsx
+++ src/components/MDX/YouWillLearnCard.tsx
@@ -31,7 +31,6 @@ function YouWillLearnCard({title, path, children}: YouWillLearnCardProps) {
 <<<<<<< HEAD
           Seguir Leyendo
           <IconNavArrow displayDirection="end" className="inline ms-1" />
->>>>>>> 819518cfe32dd2db3b765410247c30feea713c77
         </ButtonLink>
       </div>
     </div>
