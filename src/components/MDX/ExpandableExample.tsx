c2c1480a59b60acb3455d927a63e7001d6e91b03
Néstor Guillermo Hernández <nghsoft@fibertel.com.ar>
2023-08-14 15:02:57 -0300

    Update src/components/MDX/ExpandableExample.tsx

--- src/components/MDX/ExpandableExample.tsx
+++ src/components/MDX/ExpandableExample.tsx
@@ -81,7 +81,7 @@ function ExpandableExample({children, excerpt, type}: ExpandableExampleProps) {
               Ejemplo
 =======
               <IconCodeBlock className="inline me-2 dark:text-yellow-30 text-yellow-50" />
-              Example
+              Ejemplo
 >>>>>>> 819518cfe32dd2db3b765410247c30feea713c77
             </>
           )}
