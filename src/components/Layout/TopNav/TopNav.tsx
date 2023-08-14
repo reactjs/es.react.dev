4e1c028ebaea82de969a0c42529711b6e65024e8
Néstor Guillermo Hernández <nghsoft@fibertel.com.ar>
2023-08-14 14:27:04 -0300

    Update src/components/Layout/TopNav/TopNav.tsx

--- src/components/Layout/TopNav/TopNav.tsx
+++ src/components/Layout/TopNav/TopNav.tsx
@@ -267,7 +267,6 @@ export default function TopNav({
                   'flex 3xl:w-[56rem] 3xl:mx-0 relative ps-4 pe-1 py-1 h-10 bg-gray-30/20 dark:bg-gray-40/20 outline-none focus:outline-link betterhover:hover:bg-opacity-80 pointer items-center text-start w-full text-gray-30 rounded-full align-middle text-base'
                 )}
                 onClick={onOpenSearch}>
-<<<<<<< HEAD
                 <IconSearch className="me-3 align-middle text-gray-30 shrink-0 group-betterhover:hover:text-gray-70" />
                 Búsqueda
                 <span className="ms-auto hidden sm:flex item-center me-1">
